6835a4062d7362cb8e54258263df3ba953bb1618
Nasif 🚀 <73707865+naszifnaaz@users.noreply.github.com>
2026-05-25 13:51:49 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        for(let row = 0; row < 9; row++) {
+            let rowMap = {};
+            for(let i = 0; i < 9; i++) {
+                let val = board[row][i];
+                 if(val == ".")
+                        continue;
+                if(rowMap[val])
+                    return false;
+                else
+                    rowMap[val] = 1;
+            }
+        }
+
+        for(let col = 0; col < 9; col++) {
+            let colMap = {};
+            for(let i = 0; i < 9; i++) {
+                let val = board[i][col];
+                 if(val == ".")
+                        continue;
+
+                if(colMap[val])
+                    return false;
+                else
+                    colMap[val] = 1;
+            }
+        }
+
+        for(let grid = 0; grid < 9; grid++) {
+            let gridMap = {};
+            for(let i = 0; i < 3; i++) {
+                for(let j = 0; j < 3; j++) {
+                    let row = Math.floor(grid / 3) * 3 + i;
+                    let col = (grid % 3) * 3 + j;
+                    let val = board[row][col];
+
+                    if(val == ".")
+                        continue;
+
+                    if(gridMap[val])
+                        return false;
+                    else
+                        gridMap[val] = 1;
+                }
+            }
+        }
+
+        return true;
+    }
+}
