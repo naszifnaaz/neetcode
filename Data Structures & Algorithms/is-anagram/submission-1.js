148d45339c18a3a56b5240c0706a35299fbe5a5c
Nasif 🚀 <73707865+naszifnaaz@users.noreply.github.com>
2026-05-25 13:32:49 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length != t.length)
+            return false;
+        
+        let map = {};
+        for(let i = 0; i < s.length; i++) {
+            if(map[s[i]])
+                map[s[i]] += 1;
+            else
+                map[s[i]] = 1;
+        }
+
+        for(let i = 0; i < t.length; i++) {
+            map[t[i]] -= 1;
+        }
+
+        for(let key in map) {
+            if(map[key] != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
