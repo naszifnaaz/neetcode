559c7bcd96519074d6ed374f6ce573235158305e
Nasif 🚀 <73707865+naszifnaaz@users.noreply.github.com>
2026-07-31 21:39:07 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = {};
+        for(let i = 0; i < strs.length; i++) {
+            let sorted_text = strs[i].split("").sort().join("");
+
+            if(map[sorted_text])
+                map[sorted_text].push(strs[i]);
+            else
+                map[sorted_text] = [strs[i]];
+        }
+
+        return Object.values(map);
+    }
+}
